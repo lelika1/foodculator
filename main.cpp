e15f91d5590b4b5fa97150f47fb8e9f42237907f
Olga Sycheva <lelika1@gmail.com>
2020-04-04 21:43:14 +0200

    Serve files via a mount point function.

--- main.cpp
+++ main.cpp
@@ -9,23 +9,17 @@ int main() {
 
     srv.Get("/", [](const httplib::Request& req, httplib::Response& res) {
         std::ifstream in("static/index.html");
-        std::string str((std::istreambuf_iterator<char>(in)),
-                        std::istreambuf_iterator<char>());
+        std::string str{std::istreambuf_iterator<char>(in),
+                        std::istreambuf_iterator<char>()};
+        in.close();
         res.set_content(str, "text/html");
-        in.close();
-    });
-
-    srv.Get("/static/icon.svg", [](const httplib::Request &req, httplib::Response &res) {
-        std::ifstream in("static/icon.svg");
-        std::string str((std::istreambuf_iterator<char>(in)),
-                        std::istreambuf_iterator<char>());
-        res.set_content(str, "image/svg+xml");
-        in.close();
     });
 
     srv.Get("/stop", [&srv](const httplib::Request& req, httplib::Response& res) {
         srv.stop();
     });
+
+    srv.set_mount_point("/static", "./static");
 
     std::cout << "Listening on http://localhost:1234" << std::endl;
     srv.listen("localhost", 1234);
