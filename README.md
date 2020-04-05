8fba34e5609227eebdeaed0c701a3fd81d13de9c
Olga Sycheva <lelika1@gmail.com>
2020-04-05 17:36:07 +0200

    Add a form to add ingredients.

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # foodculator
+
+sudo apt install libsqlite3-dev
+
+clang++ main.cpp -O2 -o build/foodculator --std=c++17 -lpthread
--- main.cpp
+++ main.cpp
@@ -1,27 +1,81 @@
-#include "cpp-httplib/httplib.h"
+#include <sqlite3.h>
+
 #include <fstream>
 #include <iostream>
 #include <streambuf>
 #include <string>
 
+#include "cpp-httplib/httplib.h"
+
+std::vector<std::string> Split(const std::string& str,
+                               const std::string& delimiter) {
+  std::vector<std::string> result;
+  size_t start = 0;
+  auto end = str.find(delimiter);
+  while (end != std::string::npos) {
+    result.push_back(str.substr(start, end - start));
+    start = end + delimiter.length();
+    end = str.find(delimiter, start);
+  }
+  result.push_back(str.substr(start, end));
+  return result;
+}
+
 int main() {
-    httplib::Server srv;
+  httplib::Server srv;
 
-    srv.Get("/", [](const httplib::Request& req, httplib::Response& res) {
-        std::ifstream in("static/index.html");
-        std::string str{std::istreambuf_iterator<char>(in),
-                        std::istreambuf_iterator<char>()};
-        in.close();
-        res.set_content(str, "text/html");
-    });
+  srv.Get("/", [](const httplib::Request& req, httplib::Response& res) {
+    std::ifstream in("static/index.html");
+    std::string str{std::istreambuf_iterator<char>(in),
+                    std::istreambuf_iterator<char>()};
+    in.close();
+    res.set_content(str, "text/html");
+  });
 
-    srv.Get("/stop", [&srv](const httplib::Request& req, httplib::Response& res) {
-        srv.stop();
-    });
+  srv.Get("/addingredient",
+          [](const httplib::Request& req, httplib::Response& res) {
+            std::ifstream in("static/ingredients.html");
+            std::string str{std::istreambuf_iterator<char>(in),
+                            std::istreambuf_iterator<char>()};
+            in.close();
+            res.set_content(str, "text/html");
+          });
 
-    srv.set_mount_point("/static", "./static");
+  srv.Post("/addingredient",
+           [](const httplib::Request& req, httplib::Response& res) {
+             std::unordered_map<std::string, std::string> params;
+             for (const auto& e : Split(req.body, "&")) {
+               auto element = Split(e, "=");
+               if (element.size() != 2) {
+                 res.status = 400;
+                 return;
+               }
 
-    std::cout << "Listening on http://localhost:1234" << std::endl;
-    srv.listen("localhost", 1234);
-    return 0;
+               params[element[0]] = element[1];
+             }
+
+             if (params["product"].empty() || params["kcal"].empty()) {
+               res.set_content(
+                   "An ingredient wasn't added. Some information is missing.",
+                   "text/plain");
+               res.status = 500;
+               return;
+             };
+
+             uint32_t kcal = std::stoi(params["kcal"]);
+             std::string response = "A new ingredient '" + params["product"] +
+                                    "' with " + std::to_string(kcal) +
+                                    "kcal for 100g was added.";
+             res.set_content(response, "text/plain");
+           });
+
+  srv.Get("/stop", [&srv](const httplib::Request& req, httplib::Response& res) {
+    srv.stop();
+  });
+
+  srv.set_mount_point("/static", "./static");
+
+  std::cout << "Listening on http://localhost:1234" << std::endl;
+  srv.listen("localhost", 1234);
+  return 0;
 }
